7807f84ceb9981323a87f86ea247080d272eab55
ashutosh gaurav <ashu00665@gmail.com>
2025-03-27 14:50:24 +0530

    P-1 Updataed UX for landing Page

--- components/Home/ThisSiteCantBeReached/ThisCantBeReached.tsx
+++ components/Home/ThisSiteCantBeReached/ThisCantBeReached.tsx
@@ -34,7 +34,7 @@ export default function ThisCantBeReached() {
       initial={{ opacity: 1 }}
       animate={{ opacity: 0 }}
       transition={{ delay: 4, duration: 0.5 }}
-      className="absolute h-screen w-full bg-white 
+      className="absolute h-screen w-full bg-[#0a172b]
       py-16 sm:py-40 
       px-4 sm:px-24 md:px-36 lg:px-48 xl:px-96 
       flex flex-col space-y-5 sm:space-y-10
@@ -47,10 +47,10 @@ export default function ThisCantBeReached() {
           animate={{ y: CenterHeight, x: CenterWidth, scale: 2 }}
           transition={{ delay: 2, duration: 1 }}
           className="relative w-9 h-10 ">
-          <div className="absolute h-1 w-1/2 bg-gray-600"></div>
-          <div className="absolute h-full w-1 bg-gray-600"></div>
-          <div className="absolute bottom-0 h-1 w-full bg-gray-600"></div>
-          <div className="absolute right-0 bottom-0 h-6 w-1 bg-gray-600"></div>
+          <div className="absolute h-1 w-1/2 bg-AAsecondary"></div>
+          <div className="absolute h-full w-1 bg-AAsecondary"></div>
+          <div className="absolute bottom-0 h-1 w-full bg-AAsecondary"></div>
+          <div className="absolute right-0 bottom-0 h-6 w-1 bg-AAsecondary"></div>
 
           {/* Left Eye */}
 
@@ -75,7 +75,7 @@ export default function ThisCantBeReached() {
               ],
             }}
             transition={{ scaleY: { delay: 1.5, duration: 1 } }}
-            className="absolute left-2 top-3 h-1.5 w-[3.5px] bg-gray-600"
+            className="absolute left-2 top-3 h-1.5 w-[3.5px] bg-AAsecondary"
           ></motion.div>
 
           {/* Right Eye */}
@@ -105,7 +105,7 @@ export default function ThisCantBeReached() {
               opacity: { delay: 1, duration: 0 },
               scaleY: { delay: 1.5, duration: 1 },
             }}
-            className="absolute right-2 top-3 h-1.5 w-[3.5px] bg-gray-600"
+            className="absolute right-2 top-3 h-1.5 w-[3.5px] bg-AAsecondary"
           ></motion.div>
 
           {/* Corner */}
@@ -117,7 +117,7 @@ export default function ThisCantBeReached() {
               x: { delay: 0.5, duration: 0.5 },
               y: { delay: 0.5, duration: 0.5 },
             }}
-            className="absolute right-4 top-0 h-[18px] w-1 bg-gray-600"
+            className="absolute right-4 top-0 h-[18px] w-1 bg-AAsecondary"
           ></motion.div>
           <motion.div
             animate={{ rotate: 90, x: 6, y: -7 }}
@@ -126,7 +126,7 @@ export default function ThisCantBeReached() {
               x: { delay: 0.5, duration: 0.5 },
               y: { delay: 0.5, duration: 0.5 },
             }}
-            className="absolute right-0 top-[14px] h-1 w-4 bg-gray-600"
+            className="absolute right-0 top-[14px] h-1 w-4 bg-AAsecondary"
           ></motion.div>
           <motion.div
             initial={{ opacity: "100%" }}
@@ -134,26 +134,26 @@ export default function ThisCantBeReached() {
             transition={{ opacity: { delay: 0.5, duration: 0 } }}
             className=""
           >
-            <div className="absolute right-3 top-0 h-1 w-1 bg-gray-600"></div>
-            <div className="absolute right-0 top-[10px] h-1 w-1 bg-gray-600"></div>
-            <div className="absolute right-1 top-[7px] h-[4px] w-[4px] bg-gray-600"></div>
-            <div className="absolute right-2 top-[4px] h-[4px] w-[4px] bg-gray-600"></div>
+            <div className="absolute right-3 top-0 h-1 w-1 bg-AAsecondary"></div>
+            <div className="absolute right-0 top-[10px] h-1 w-1 bg-AAsecondary"></div>
+            <div className="absolute right-1 top-[7px] h-[4px] w-[4px] bg-AAsecondary"></div>
+            <div className="absolute right-2 top-[4px] h-[4px] w-[4px] bg-AAsecondary"></div>
           </motion.div>
 
           {/* Smile */}
 
-          <div className="absolute left-3 bottom-[10px] w-3 h-[3px] bg-gray-600"></div>
+          <div className="absolute left-3 bottom-[10px] w-3 h-[3px] bg-AAsecondary"></div>
           <motion.div
             animate={{ y: [0, -5, 0, -5, 0, -5,] }}
             transition={{ y: { delay: 3, duration: 0.5 } }}
-            className="absolute left-[9px] bottom-[7px] w-[3px] h-[3px] bg-gray-600"
+            className="absolute left-[9px] bottom-[7px] w-[3px] h-[3px] bg-AAsecondary"
           ></motion.div>
           <motion.div
             animate={{
               y: [0, -5, 0, -5, 0, -5, 0, -5,],
             }}
             transition={{ y: { delay: 3, duration: 0.5 } }}
-            className="absolute right-[9px] bottom-[7px] w-[3px] h-[3px] bg-gray-600"
+            className="absolute right-[9px] bottom-[7px] w-[3px] h-[3px] bg-AAsecondary"
           ></motion.div>
           {/* ! Hello animation text */}
         </motion.div>
@@ -161,7 +161,7 @@ export default function ThisCantBeReached() {
           initial={{ y: CenterHeight + 50 - 20, x: CenterWidth - 13, opacity: 0 }}
           animate={{ y: CenterHeight + 50, opacity: 1 }}
           transition={{ delay: 3.5, duration: 0.3 }}
-          className="absolute font-bold text-gray-600 text-2xl">
+          className="absolute font-bold text-AAsecondary text-2xl">
           Hello!
         </motion.span>
         <motion.span
@@ -179,23 +179,23 @@ export default function ThisCantBeReached() {
           transition={{ opacity: { delay: 2, duration: 0.5 } }}
           className="w-full  flex flex-col space-y-8"
         >
-          <span className="text-gray-600 font-Header text-2xl">
+          <span className="text-AAsecondary font-Header text-2xl">
             This site{" "}
             {ShowText ? (
               <motion.span
                 animate={{ scale: ["100%", "120%"] }}
                 transition={{ scale: { delay: 4, duration: 0.5 } }}
-                className="font-bold"
+                className="font-bold "
               >
-                actually can
+                <span className="font-bold text-white">actually can</span>
               </motion.span>
             ) : (
-              <span>can&apos;t</span>
+              <span className="text-white">can&apos;t</span>
             )}{" "}
             be reached
           </span>
-          <span className="text-gray-500 text-md">
-            <span className="font-bold">www.ashuPort</span>
+          <span className="text-AAsecondary text-md">
+            <span className="font-bold">www.ashuPort </span>
             unexpectedly{" "}
             {ShowText ? (
               <motion.span
@@ -211,17 +211,17 @@ export default function ThisCantBeReached() {
             the connection.
           </span>
           <div className="flex flex-col space-y-3">
-            <span className="font-Header text-gray-400 text-lg">Try:</span>
+            <span className="font-Header text-AAsecondary text-lg">Try:</span>
             <div className="flex flex-col space-y-2 pl-10">
-              <span className="text-Header text-gray-400 font-Header sm:text-base text-sm">
+              <span className="text-Header text-white font-Header sm:text-base text-sm">
                 <span className="text-bold text-gray-500">&bull;</span> Checking
                 the connection
               </span>
-              <span className="text-Header text-blue-500 font-Header sm:text-base text-sm">
+              <span className="text-Header text-white font-Header sm:text-base text-sm">
                 <span className="text-bold text-gray-500">&bull;</span> Checking
                 the proxy and the firewall
               </span>
-              <span className="text-Header text-blue-500 font-Header sm:text-bdase text-sm">
+              <span className="text-Header text-white font-Header sm:text-bdase text-sm">
                 <span className="text-bold text-gray-500">&bull;</span> Running
                 Windows Network Diagnostics
               </span>
@@ -232,7 +232,7 @@ export default function ThisCantBeReached() {
               <motion.span
                 animate={{ scale: ["100%", "120%"] }}
                 transition={{ scale: { delay: 4, duration: 0.5 } }}
-                className="font-bold"
+                className="font-bold text-amber-50"
               >
                 SUCC_CONNECTION_OPENED
               </motion.span>
